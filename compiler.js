612b63a1bd1e75d2fa3bdb579ce122488d8e05d1
Ruurd Bijlsma <ruurd@snakeware.nl>
2018-05-17 10:25:50 +0200

    Initial Commit

---
+++ compiler.js
@@ -0,0 +1,87 @@
+compile = input => {
+	let result = `
+input => {
+    let cells = {0:0};
+    let index = 0;
+    let output = '';
+
+	let shift = delta => {
+		index += delta;
+		cells[index] = cells[index] || 0;
+	};
+
+	let add = delta => {
+		cells[index] += delta;
+	};
+
+`;
+
+	let allowedTokens = '<>+-.,[]';
+
+	let actions = {
+        '.': () => `output += String.fromCharCode(cells[index])`,
+        ',': () => `cells[index] = prompt()`,
+        '[': () => `while(cells[index]){`,
+        ']': () => '}'
+    }
+
+	input = input.split('').filter(char => allowedTokens.includes(char)).join('');
+
+	let indentation = 1;
+	for(let i = 0; i < input.length; i++){
+		if(input[i] === ']')
+			indentation--;
+		let line = '\t'.repeat(indentation);
+		if(input[i] === '[')
+			indentation++;
+
+		if(input[i] === '+' || input[i] === '-'){
+
+            let addition = 0;
+            while(input[i] === '+' || input[i] === '-'){
+                if(input[i] === '+')
+                    addition++;
+                if(input[i] === '-')
+                    addition--;
+                i++;
+            }
+
+			line += `add(${addition})`;
+
+			i--;
+
+        }
+		else if(input[i] === '<' || input[i] === '>'){
+
+            let shift = 0;
+            while(input[i] === '<' || input[i] === '>'){
+                if(input[i] === '>')
+                    shift++;
+                if(input[i] === '<')
+                    shift--;
+                i++;
+            }
+
+			line += `shift(${shift})`;
+
+			i--;
+
+        } else {
+			line += actions[input[i]]();
+        }
+
+		result += line + '\n';
+    }
+
+	result += `
+    console.log(output);
+}
+	`;
+
+	console.log(result);
+	return eval(result);
+}
+
+p = compile(`++++++++++[>++++++++++<-]>>++++++++++>->>>>>>>>>>>>>>>>-->+++++++[->++++++++++<]>[->+>+>+>+<<<<]+++>>+++>>>++++++++[-<++++<++++<++++>>>]+++++[-<++++<++++>>]>>-->++++++[->+++++++++++<]>[->+>+>+>+<<<<]+++++>>+>++++++>++++++>++++++++[-<++++<++++<++++>>>]++++++[-<+++<+++<+++>>>]>>-->---+[-<+]-<[+[->+]-<<->>>+>[-]++[-->++]-->+++[---++[--<++]---->>-<+>[++++[----<++++]--[>]++[-->++]--<]>++[--+[-<+]->>[-]+++++[---->++++]-->[->+<]>>[.>]++[-->++]]-->+++]---+[-<+]->>-[+>>>+[-<+]->>>++++++++++<<[->+>-[>+>>]>[+[-<+>]>+>>]<<<<<<]>>[-]>>>++++++++++<[->-[>+>>]>[+[-<+>]>+>>]<<<<<]>[-]>>[>++++++[-<++++++++>]<.<<+>+>[-]]<[<[->-<]++++++[->++++++++<]>.[-]]<<++++++[-<++++++++>]<.[-]<<[-<+>]+[-<+]->>]+[-]<<<.>>>+[-<+]-<<]`);
+
+p();
---
+++ interpreter.js
@@ -0,0 +1,56 @@
+brainfuck = input => {
+    let cells = { 0: 0 };
+    let index = 0;
+    let output = '';
+    let actions = {
+        '>': () => cells[++index] = cells[index] || 0,
+        '<': () => cells[--index] = cells[index] || 0,
+        '+': () => cells[index]++,
+        '-': () => cells[index]--,
+        '.': () => output += String.fromCharCode(cells[index]),
+        ',': () => cells[index] = prompt(),
+        '[': (input, i) => {
+            if (!cells[index]) {
+                let ignores = 0;
+                for (let n = i + 1; n < input.length; n++) {
+                    if (input[n] === '[')
+                        ignores++;
+                    if (input[n] === ']' && ignores-- === 0)
+                        return n + 1;
+                }
+            }
+        },
+        ']': (input, i) => {
+            if (cells[index]) {
+                let ignores = 0;
+                for (let n = i - 1; n >= 0; n--) {
+                    if (input[n] === ']')
+                        ignores++;
+                    if (input[n] === '[' && ignores-- === 0)
+                        return n + 1;
+                }
+            }
+        }
+    }
+
+    input = input.split('').filter(char => actions[char]).join('');
+
+    let iterationsLeft = 10000;
+    for (let i = 0; i < input.length; i++) {
+        let char = input[i];
+        if (!actions[char])
+            continue;
+
+        let newIndex = actions[char](input, i);
+        if ((char === ']' || char === '[') && newIndex) {
+            i = newIndex - 1;
+        }
+
+        if (iterationsLeft-- <= 0) {
+            break;
+        }
+    }
+    return output;
+}
+
+brainfuck(`++++++++++[>++++++++++<-]>>++++++++++>->>>>>>>>>>>>>>>>-->+++++++[->++++++++++<]>[->+>+>+>+<<<<]+++>>+++>>>++++++++[-<++++<++++<++++>>>]+++++[-<++++<++++>>]>>-->++++++[->+++++++++++<]>[->+>+>+>+<<<<]+++++>>+>++++++>++++++>++++++++[-<++++<++++<++++>>>]++++++[-<+++<+++<+++>>>]>>-->---+[-<+]-<[+[->+]-<<->>>+>[-]++[-->++]-->+++[---++[--<++]---->>-<+>[++++[----<++++]--[>]++[-->++]--<]>++[--+[-<+]->>[-]+++++[---->++++]-->[->+<]>>[.>]++[-->++]]-->+++]---+[-<+]->>-[+>>>+[-<+]->>>++++++++++<<[->+>-[>+>>]>[+[-<+>]>+>>]<<<<<<]>>[-]>>>++++++++++<[->-[>+>>]>[+[-<+>]>+>>]<<<<<]>[-]>>[>++++++[-<++++++++>]<.<<+>+>[-]]<[<[->-<]++++++[->++++++++<]>.[-]]<<++++++[-<++++++++>]<.[-]<<[-<+>]+[-<+]->>]+[-]<<<.>>>+[-<+]-<<]`)
